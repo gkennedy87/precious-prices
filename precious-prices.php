933781f952654855126045d2f0ca5b244c58d094
Grant Kennedy <grant@gmkennedy.com>
2021-07-11 21:09:43 +0900

    initial commit

---
+++ precious-prices.php
@@ -0,0 +1,26 @@
+<?php
+/*
+    @package PreciousPrices
+
+    Plugin Name: Precious Prices
+    Plugin URI: https://www.gmkennedy.com
+    Description: Automatically update gold and silver product prices in your Woocommerce store
+    Version: 1.0.0
+    Author: Grant Kennedy
+    Author URI: https://www.gmkennedy.com
+    License: GPLv3 or later
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
